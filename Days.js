e4fad91ffe780b65b2361d762c9b28a24ca61a8f
Chittamur Diwakar Reddy <39544216+dillipc12@users.noreply.github.com>
2022-11-29 07:19:29 +0530

    Update Days.js

--- Days.js
+++ Days.js
@@ -1,5 +1,17 @@
 var choice=12;
 switch(choice)
 {
-        case 
+        case 4:
+        case 8:
+        case 9:
+        case 11: 
+                console.log("30 Days");
+                break;
+        case 2:
+                console.log("28/29 Days");
+                break;
+        default:
+                console.log("31 Days");
+                break;
+
 }
